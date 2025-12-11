26410e309f4f05e1143ee8c2ebfdb8caa88bc0d1
I-am-nagesh <i.am.nageshpandey@gmail.com>
2025-12-11 08:13:07 +0530

    log parser done

--- python/log_parser.py
+++ python/log_parser.py
@@ -11,10 +11,34 @@ send email
 # for log in logfile:
 #     print(log)
 
+# with open("serverlogs.log", "r") as file:
+#     for line in file:
+#         #getting IP address(split by space, take the first part)
+#         parts = line.split(" ")
+#         ip_address = parts[0]
+
+#         print(f"found IP: {ip_address}")
+
+
 with open("serverlogs.log", "r") as file:
     for line in file:
-        #getting IP address(split by space, take the first part)
+        
+        #getting IP
         parts = line.split(" ")
-        ip_address = parts[0]
+        ip = parts[0]
+        
+        #split the wholw line by word "requestsuccessful:"
+        temp_part = line.split("requestsuccessful:")
 
-        print(f"found IP: {ip_address}")
+        if len(temp_part) > 1:
+            remainder = temp_part[1]     # this is 2nd part
+
+            #split the remainder by "requestfailed:"
+            numbers = remainder.split("requestfailed:")
+
+            success_count = numbers[0].strip()    #remove extra space
+            failed_count = numbers[1].strip()     #remove extra space
+
+            print(f"IP: {ip} | Success: {success_count} | Failed: {failed_count}")
+
+
